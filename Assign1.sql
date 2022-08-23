3675350a4d6cd4cfc9d016601227c13701dcf57f
Prashant Sanjay Pagare <104510947+prashantpagaree@users.noreply.github.com>
2022-08-23 16:16:35 +0530

    Add files via upload

---
+++ Assign1.sql
@@ -0,0 +1,18 @@
+create database Lilavati_Hospital;
+USE Lilavati_Hospital;
+create table hospital(
+customer_id int primary key,
+patient_id int);
+
+create database Movies_by_Dharma_production; 
+use Movies_by_Dharma_production; 
+create table movie1(
+customer_name int primary key,
+movie_id int);
+
+
+create database SBI; 
+use SBI; 
+create table bank(
+customer_id int primary key,
+credit_id int);
