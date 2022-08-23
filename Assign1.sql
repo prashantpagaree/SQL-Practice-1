3acc3e71167bb574e2ea72b9a6b51dbd3c728ce7
Prashant Sanjay Pagare <104510947+prashantpagaree@users.noreply.github.com>
2022-08-23 16:24:29 +0530

    Update Assign1.sql

--- Assign1.sql
+++ Assign1.sql
@@ -1,18 +1,32 @@
+--create database Lilavati_Hospital
 create database Lilavati_Hospital;
-USE Lilavati_Hospital;
+
+--use database Lilavati_Hospital
+use Lilavati_Hospital;
+
+--create table hospital
 create table hospital(
 customer_id int primary key,
 patient_id int);
 
-create database Movies_by_Dharma_production; 
+--create database Movies_by_Dharma_production
+create database Movies_by_Dharma_production;
+
+--use database Movies_by_Dharma_production
 use Movies_by_Dharma_production; 
+
+--create table movie1
 create table movie1(
 customer_name int primary key,
 movie_id int);
 
-
+--create database SBI
 create database SBI; 
+
+--use database SBI
 use SBI; 
+
+--create table bank
 create table bank(
 customer_id int primary key,
 credit_id int);
