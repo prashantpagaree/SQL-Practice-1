2c14ede9ed503bc83e5b450a2d3ed4b2edb41dc3
Prashant Sanjay Pagare <104510947+prashantpagaree@users.noreply.github.com>
2022-08-23 16:45:55 +0530

    Update Assignment -3O.sql

--- Assignment -3O.sql
+++ Assignment -3O.sql
@@ -1,3 +1,4 @@
+--create database
 CREATE DATABASE A3O;
 USE A3O;
 
@@ -17,10 +18,12 @@ INSERT INTO A3O VALUES(1,'Geetika','Chauhan','MHOW','India',453441),
 (3,'Nitin','Chauhan','New York','USA',666784),
 (4,'Naincy','Raj','New Jersy','USA',789442);
 
---2. Insert a new column with name state in this table.
+--2. Insert a new column with name state in this table.
+
 alter table [dbo].[A3O] add state char(25);
 
---3. Insert values in the newly created ìstateî column.
+--3. Insert values in the newly created ‚Äústate‚Äù column.
+
 update A3O set state='mah' where customerid=1;
 update A3O set state='GJ' where customerid=2;
 update A3O set state='MP' where customerid=3;
@@ -30,14 +33,28 @@ update A3O set state='UP' where customerid=4;
 
 insert into A3O values(5,'Prashant','Pagare','Shirdi','India',423605,'MH')
 
---5. Delete the row which has the city Indore.
+--5. Delete the row which has the city Indore.
+
 DELETE FROM A3O WHERE CITY = 'INDORE';
 
 --6. For Customer Geetika, update the city to Indore
 
 update A3O set city='Indore' where First_Name='Geetika';
 
---7. For Customerid = 4, update city = Lucknow, state = UP and country = India.update A3O set city='Lucknow',state='UP',country='India' where CUSTOMERID=4;--8. Delete the records where country = USA. delete from A3O WHERE COUNTRY = 'USA';--9. Delete the entire table. drop table A3O;
+--7. For Customerid = 4, update city = Lucknow, state = UP and country = India.
+
+update A3O set city='Lucknow',state='UP',country='India' where CUSTOMERID=4;
+
+--8. Delete the records where country = USA. 
+
+delete from A3O WHERE COUNTRY = 'USA';
+
+--9. Delete the entire table. 
+
+drop table A3O;
+
+
+
 
 
 
